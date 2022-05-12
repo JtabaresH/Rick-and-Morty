a5c3c0a4fedd2dcf1ca123b8c3873de20b0b3236
JtabaresH <jtabaresh@gmail.com>
2022-05-12 14:09:01 -0500

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -3,8 +3,26 @@ import React from 'react';
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      {/* Aquí inicia el header */}
+      <nav className="navbar navbar-light bg-light">
+        <div className="container-fluid">
+          <a href="/" className="navbar-brand">
+            Rick Sanchez -APP- Morty Smith
+          </a>
+          <form className="d-flex">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </nav>
+      {/* Aquí finaliza el header */}
     </div>
   );
 }
