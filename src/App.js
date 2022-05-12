724ea6fad806d0e130f08804308e43b8e4cc95c1
JtabaresH <jtabaresh@gmail.com>
2022-05-12 17:04:20 -0500

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,13 +2,10 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Characters from './components/Characters';
 
-
 export default function App() {
-
-
   return (
     <>
-      <Navbar title="Rick Sanchez -APP- Morty Smith" />
+      <Navbar title="Universes of Rick and Morty" />
       <Characters />
     </>
   );
