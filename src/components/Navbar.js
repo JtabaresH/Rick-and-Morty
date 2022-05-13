43cd9cae4a00abed5a71cd694d32261700aecfa7
JtabaresH <jtabaresh@gmail.com>
2022-05-12 20:48:01 -0500

    2 files modified

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,16 +8,7 @@ const Navbar = ({ title }) => {
           <a href="/" className="navbar-brand">
             <h1>{title}</h1>
           </a>
-          <form className="d-flex">
-            <input
-              className="form-control me-2"
-              type="number"
-              min="1"
-              max="126"
-              placeholder="Input ID"
-            />
-            <button className="btn btn-outline-success">Search</button>
-          </form>
+          
         </div>
       </nav>
     </div>
--- src/components/Universe.js
+++ src/components/Universe.js
@@ -5,7 +5,9 @@ const Universe = () => {
   /* Returns a random number between min (included) and max (excluded) */
   const ramdomizer = Math.floor(Math.random() * (127 - 1)) + 1;
   const [location, setLocation] = useState([]);
+  const [id, setID] = useState();
   const [charactersURL, setCharactersURL] = useState([]);
+  const [character, setCharacter] = useState([]);
 
   useEffect(() => {
     axios
@@ -15,26 +17,43 @@ const Universe = () => {
         setCharactersURL(res.data.residents);
       });
   }, []);
+
+  const searchUniverse = () => {
+    axios.get(`https://rickandmortyapi.com/api/location/${id}`).then((res) => {
+      setLocation(res.data);
+    });
+  };
+
   console.log(location);
-  console.log(charactersURL);
 
   return (
     <div className="container">
+      <div>
+        <input
+          className="form-control me-2"
+          type="number"
+          min="1"
+          max="126"
+          step="1"
+          onChange={(e) => setID(e.target.value)}
+          placeholder="Input ID"
+        />
+        <button className="btn btn-outline-success" onClick={searchUniverse}>
+          Search
+        </button>
+      </div>
+
       <h1>{location.name}</h1>
       <span>
         <b>Type:</b> {location.type} <b>Dimension:</b> {location.dimension}{' '}
         <b>Population:</b> {location.residents?.length}
       </span>
 
-      <div className="row">
-        {charactersURL.map((item, index) => (
-          <div key={index} className="col">
-            <div className="card">
-              <img src={item.image} alt="" />
-            </div>
-          </div>
+      <ul>
+        {charactersURL?.map((URLs) => (
+          <li key={URLs}>{URLs}</li>
         ))}
-      </div>
+      </ul>
     </div>
   );
 };
