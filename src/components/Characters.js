9d5a69367e3af05e1a55dba42fe553ce658f99f5
JtabaresH <jtabaresh@gmail.com>
2022-05-12 21:43:45 -0500

    1 file modified

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -2,17 +2,18 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Characters = ({ url }) => {
-  const [character, setCharacter] = useState([]);
+  const [character, setCharacter] = useState({});
 
   useEffect(() => {
     axios.get(url).then((res) => {
       setCharacter(res.data);
     });
   }, []);
-
+  console.log(character);
   return (
     <div>
       <h1>{character.name}</h1>
+      <img src={character.image} alt="" />
     </div>
   );
 };
