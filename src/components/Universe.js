bbdc8b9e3f576494af7006f61b21626d5c8eb6be
JtabaresH <jtabaresh@gmail.com>
2022-05-13 20:53:22 -0500

    1 file modified

--- src/components/Universe.js
+++ src/components/Universe.js
@@ -7,6 +7,7 @@ const Universe = () => {
   const ramdomizer = Math.floor(Math.random() * (127 - 1)) + 1;
   const [location, setLocation] = useState([]);
   const [id, setID] = useState();
+  const [id2, setID2] = useState();
   const [charactersURL, setCharactersURL] = useState([]);
 
   useEffect(() => {
@@ -25,9 +26,16 @@ const Universe = () => {
     });
   };
 
+  const searchUniverse2 = () => {
+    axios.get(`https://rickandmortyapi.com/api/location/${id2}`).then((res) => {
+      setLocation(res.data);
+      setCharactersURL(res.data.residents);
+    });
+  };
+
   useEffect(() => {
-    searchUniverse();
-  }, [id]);
+    searchUniverse2();
+  }, [id2]);
 
   return (
     <div className="container">
@@ -41,8 +49,7 @@ const Universe = () => {
           min="1"
           max="126"
           step="1"
-          value={id}
-          onChange={(e) => setID(e.target.value)}
+          onChange={(e) => setID2(e.target.value)}
           placeholder="Input ID"
         />
         <span>
@@ -53,7 +60,6 @@ const Universe = () => {
           type="number"
           min="1"
           max="126"
-          value={id}
           onChange={(e) => setID(e.target.value)}
           placeholder="Input ID"
         />
@@ -70,7 +76,7 @@ const Universe = () => {
       </span>
       </div>
 
-      <div className="row" style={{ gap: '10px' }}>
+      <div className="row" style={{ gap: '10px', justifyContent: "center" }}>
         {charactersURL?.map((URLs) => (
           <Characters url={URLs} key={URLs} />
         ))}
