2b6f1980201d31271d684201a9f0d96065fa1690
JtabaresH <jtabaresh@gmail.com>
2022-07-31 17:05:45 -0500

    2 files modified

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Navbar = ({ title }) => {
   return (
-    <div style={{backgroundColor: "rgb(29, 16, 92)"}}>
+    <div style={{ backgroundColor: '#000000)' }}>
       <nav>
         <div className="container-fluid">
-          <h1 style={{ textAlign: 'center', color: "white" }}>{title}</h1>
+          <h1 style={{ textAlign: 'center', color: 'white' }}>{title}</h1>
         </div>
       </nav>
     </div>
